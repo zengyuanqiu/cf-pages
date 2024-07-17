b2afeba16118b95a3e01784b27d86c78f1639c3c
zou <251881715@qq.com>
2024-07-17 15:48:37 +0800

    use fetch request

--- functions/data.js
+++ functions/data.js
@@ -3,9 +3,9 @@
  * @Author: zengyuanqiu
  * @Date: 2024-07-17 15:06:11
  * @LastEditors: zengyuanqiu
- * @LastEditTime: 2024-07-17 15:08:59
+ * @LastEditTime: 2024-07-17 15:45:13
  */
 export function onRequest(context) {
   console.log('已执行')
-  return new Response('Hello, world!')
+  return new Response('{"code": 0, "data": {"id": 1}, "msg": "success"}')
 }
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
  * @Author: zengyuanqiu
  * @Date: 2024-04-20 11:33:58
  * @LastEditors: zengyuanqiu
- * @LastEditTime: 2024-07-17 14:10:36
+ * @LastEditTime: 2024-07-17 15:47:29
 -->
 <template>
   <div class="app">
@@ -16,6 +16,31 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+
+fetch('./data')
+  .then(res => {
+    try {
+      return res.json()
+    } catch (e) {
+      return res.text()
+    }
+  })
+  .then(res => {
+    console.log(27)
+    console.log(res)
+  })
+fetch('/data')
+  .then(res => {
+    try {
+      return res.json()
+    } catch (e) {
+      return res.text()
+    }
+  })
+  .then(res => {
+    console.log(27)
+    console.log(res)
+  })
 </script>
 
 <style scoped></style>
