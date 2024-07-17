c811631e41366441387fc75fa4baf39a6daa1b34
zou <251881715@qq.com>
2024-07-17 15:08:11 +0800

    use funtions

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion:
+ * @Author: zengyuanqiu
+ * @Date: 2024-07-17 15:06:11
+ * @LastEditors: zengyuanqiu
+ * @LastEditTime: 2024-07-17 15:07:46
+ */
+export function onRequest(context) {
+  return new Response('Hello, world!')
+}
