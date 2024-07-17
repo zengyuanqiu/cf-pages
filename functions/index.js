e0878a871b5a912e7277cc14708831fef8870d0f
zou <251881715@qq.com>
2024-07-17 15:10:05 +0800

    use functions

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,9 @@
  * @Author: zengyuanqiu
  * @Date: 2024-07-17 15:06:11
  * @LastEditors: zengyuanqiu
- * @LastEditTime: 2024-07-17 15:07:46
+ * @LastEditTime: 2024-07-17 15:08:59
  */
 export function onRequest(context) {
+  console.log('已执行')
   return new Response('Hello, world!')
 }
