b59ddebc51150941e79072c75fafd4aa28e70e59
zou <251881715@qq.com>
2024-12-11 15:59:03 +0800

    添加刷新日志按钮

--- src/components/LogNd.vue
+++ src/components/LogNd.vue
@@ -1,4 +1,6 @@
 <template>
+  <button @click="getLog">刷新</button>
+  <hr />
   <ul>
     <li v-for="(t, i) in logs" :key="i">
       <div>{{ t.date }}</div>
@@ -12,16 +14,19 @@ import { ref } from 'vue'
 
 const logs = ref([])
 
-fetch('/logs/nd?r=1')
-  .then(res => res.json())
-  .then(res => {
-    if (res?.data?.length) {
-      logs.value = res.data.reverse().map(t => {
-        t.date = new Date(t.date).toLocaleString()
-        return t
-      })
-    }
-  })
+function getLog() {
+  fetch('/logs/nd?r=1')
+    .then(res => res.json())
+    .then(res => {
+      if (res?.data?.length) {
+        logs.value = res.data.reverse().map(t => {
+          t.date = new Date(t.date).toLocaleString()
+          return t
+        })
+      }
+    })
+}
+getLog()
 </script>
 
 <style scoped>
