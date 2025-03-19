66d569a8bfdea9ccb80d0fe1d55f99a9aab4b059
zou <251881715@qq.com>
2025-03-19 16:50:40 +0800

    粘性定位，溢出不撑大

--- src/components/LogNd.vue
+++ src/components/LogNd.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex-1 p-20 overflow-hidden">
-    <div class="flex">
-      <button @click="getLog">刷新</button>&nbsp;<button @click="clearLog">
-        清空日志
-      </button>
-      <div class="mx-auto pr-240">尼 斗</div>
+  <div class="flex-1 p-20 min-w-0">
+    <div class="sticky top-28 bg-[#fff] shadow-[0_-28px_0_#fff]">
+      <div class="flex">
+        <button @click="getLog">刷新</button>&nbsp;<button @click="clearLog">
+          清空日志
+        </button>
+        <div class="mx-auto pr-240">尼 斗</div>
+      </div>
+      <hr />
     </div>
-
-    <hr />
     <ul>
       <li v-for="(t, i) in logs" :key="i">
         <div>{{ t.date }}</div>
@@ -40,8 +41,11 @@ function getLog() {
 getLog()
 
 function clearLog() {
-  logs.value = []
-  fetch('/logs/nd?d=1')
+  if (!logs.value.length) return
+  if (window.confirm('确定清空日志吗')) {
+    logs.value = []
+    fetch('/logs/nd?d=1')
+  }
 }
 </script>
 
